f1bb501f01f7033fb287ed320a7275a573a77528
22-2 <17890m@protonmail.ch>
2026-03-28 01:09:57 +0900

    fix: ビルド時にワーカーファイルが生成されてしまう問題を修正

--- src/db/worker-client.ts
+++ src/db/worker-client.ts
@@ -1,5 +1,6 @@
 import * as Comlink from "comlink";
 import type { IDBService } from "src/db/interfaces/IDBService";
+import ScanWorkerFactory from "src/db/scan.worker?worker&inline";
 
 export class WorkerClient {
   private static instance: Comlink.Remote<IDBService> | null = null;
@@ -8,9 +9,7 @@ export class WorkerClient {
   static get(): Comlink.Remote<IDBService> {
     if (!this.instance) {
       // @ts-ignore - Vite handled worker import
-      this.worker = new Worker(new URL("./scan.worker.ts", import.meta.url), {
-        type: "module",
-      });
+      this.worker = new ScanWorkerFactory();
       this.instance = Comlink.wrap<IDBService>(this.worker);
     }
     return this.instance;
