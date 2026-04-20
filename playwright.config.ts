a489e0b54f36e7d840a7c0e73d077c6ade1586b8
22-2 <17890m@protonmail.ch>
2026-04-21 00:58:12 +0900

    test: Update Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  maxFailures: 1,
   workers: 1,
   reporter: "html",
   use: {
